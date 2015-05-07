eab5c413ffc17c1f15ed052026d767c4e8d7a7d5
ManGregory <manaenkov.gregory@gmail.com>
2015-05-07 11:50:48 +0300

    added module 7

--- Module7/Program.cs
+++ Module7/Program.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Module5
+namespace Module7
 {
-    class Student
+    using System;
+
+    internal class Student
     {
         private static int _count;
         private string _firstName;
@@ -14,10 +21,12 @@ namespace Module5
         private string _state;
         private string _country;
         private int _zipCode;
+        private Stack _grades;
 
         public Student()
         {
             _count++;
+            _grades = new Stack();
         }
 
         public static int Count
@@ -61,7 +70,7 @@ namespace Module5
             set { _city = value; }
         }
 
-        public string State 
+        public string State
         {
             get { return _state; }
             set { _state = value; }
@@ -78,9 +87,15 @@ namespace Module5
             get { return _zipCode; }
             set { _zipCode = value; }
         }
+
+        public Stack Grades
+        {
+            get { return _grades; }
+            set { _grades = value; }
+        }
     }
 
-    class Teacher
+    internal class Teacher
     {
         private string _firstName;
         private string _lastName;
@@ -147,7 +162,7 @@ namespace Module5
         }
     }
 
-    class Degree
+    internal class Degree
     {
         private string _name;
         private Course _course;
@@ -165,17 +180,17 @@ namespace Module5
         }
     }
 
-    class Course
+    internal class Course
     {
         private string _name;
-        private Student[] _students;
+        private ArrayList _students;
         private Teacher[] _teachers;
         private const int StudentsDefaultSize = 3;
         private const int TeachersDefaultSize = 3;
 
         public Course()
         {
-            _students = new Student[StudentsDefaultSize];
+            _students = new ArrayList(StudentsDefaultSize);
             _teachers = new Teacher[TeachersDefaultSize];
         }
 
@@ -185,11 +200,11 @@ namespace Module5
             set { _name = value; }
         }
 
-        public Student[] Students
+        public ArrayList Students
         {
             get { return _students; }
             set { _students = value; }
-        }       
+        }
 
         public Teacher[] Teachers
         {
@@ -198,7 +213,7 @@ namespace Module5
         }
     }
 
-    class UProgram
+    internal class UProgram
     {
         private string _name;
         private int _requiredCredits;
@@ -214,7 +229,7 @@ namespace Module5
         {
             get { return _requiredCredits; }
             set { _requiredCredits = value; }
-        }        
+        }
 
         public Degree Degree
         {
@@ -223,27 +238,64 @@ namespace Module5
         }
     }
 
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            var students = new[]
-            {
-                new Student {FirstName = "Petr"},
-                new Student {FirstName = "Ivan"},
-                new Student {FirstName = "Igor"}
-            };
+            var students = new ArrayList();
+            students.AddRange(CreateStudents());
+
             var course = new Course {Name = "Programming with C#", Students = students};
             var teacher = new Teacher {FirstName = "Nikiphor"};
             course.Teachers[0] = teacher;
             var degree = new Degree {Name = "Bachelor", Course = course};
             var program = new UProgram {Name = "Information Technology", Degree = degree};
 
+            // print the information about students
+            ListStudents(students);
+
+            // output from module 6
             Console.WriteLine("The {0} program contains the {1} of Science degree", program.Name, program.Degree.Name);
             Console.WriteLine("The {0} of Science degree contains the course {1}", degree.Name, degree.Course.Name);
-            Console.WriteLine("The {0} course contains {1} students", course.Name, course.Students.Length);
+            Console.WriteLine("The {0} course contains {1} students", course.Name, course.Students.Count);
             Console.WriteLine("Press any key to continue");
             Console.ReadKey();
         }
+
+        private static void ListStudents(ArrayList students)
+        {
+            Console.WriteLine("The list of students:");
+            foreach (var studentObject in students)
+            {
+                // check the object type, because ArrayList contains array of objects and we can store anything
+                var student = studentObject as Student;
+                if (student != null)
+                {
+                    Console.WriteLine("{0} {1}", student.FirstName, student.LastName);
+                }
+            }
+            Console.WriteLine();
+        }
+
+        private static Student[] CreateStudents()
+        {
+            var student1 = new Student {FirstName = "John", LastName = "Petrov"};
+            var student2 = new Student {FirstName = "Dave", LastName = "Laru"};
+            var student3 = new Student {FirstName = "Frodo", LastName = "Beggins"};
+            AddGradesToStudent(student1);
+            AddGradesToStudent(student2);
+            AddGradesToStudent(student3);
+            return new[] {student1, student2, student3};
+        }
+
+        private static void AddGradesToStudent(Student student)
+        {
+            student.Grades.Push(1);
+            student.Grades.Push(2);
+            student.Grades.Push(3);
+            student.Grades.Push(4);
+            student.Grades.Push(5);
+        }
     }
 }
+
